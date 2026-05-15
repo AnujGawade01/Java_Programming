40d545ad2575de6bd2b30e703273c3fe6bae39d4
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-05-15 09:15:54 +0530

    Update ThisSuper.java

--- ThisSuper.java
+++ ThisSuper.java
@@ -1,4 +1,4 @@
-// Here we use the this keyword to call the class method
+// Here we use the this keyword & super keyword to call the base/parent class method
 class ThisSuper
 {
     public static void main(String A[])
