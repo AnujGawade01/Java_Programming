319bf73d95027ec587d2485ccb7586de9af9194b
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-05-23 00:26:04 +0530

    Add files via upload

---
+++ ThreadScenario1X.java
@@ -0,0 +1,19 @@
+class ThreadScenario1X
+{
+    public static void main(String A[])
+    {
+        System.out.println("Main thread is running...");
+        Demo dobj = new Demo();
+        Thread tobj = new Thread(dobj);
+
+        tobj.start();
+    }
+}
+
+class Demo extends Thread
+{
+    public void run()
+    {
+        System.out.println("Inside run method");
+    }
+}
---
+++ ThreadScenario2.java
@@ -0,0 +1,19 @@
+class ThreadScenario2
+{
+    public static void main(String A[])
+    {
+        System.out.println("Main thread is running...");
+        Demo dobj = new Demo();
+        Thread tobj = new Thread(dobj);
+
+        tobj.start();
+    }
+}
+
+class Demo implements Runnable
+{
+    public void run()
+    {
+        System.out.println("Inside run method");
+    }
+}
