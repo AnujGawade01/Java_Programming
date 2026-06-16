b6a2fbebf207aa7e34a3264522f4d8c674b424f5
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-06-16 20:38:28 +0530

    Add files via upload

---
+++ GUI1.java
@@ -0,0 +1,15 @@
+// Here we created GUI Frame using swing library
+import javax.swing.*;
+
+class GUI1
+{
+    public static void main(String A[])
+    {
+        JFrame fobj = new JFrame("Marvellous PPA");
+
+        fobj.setSize(400,300);
+
+        fobj.setVisible(true);
+
+    }
+}
---
+++ GUI2.java
@@ -0,0 +1,16 @@
+// Here we created GUI Frame using awt library
+import java.awt.*;
+
+class GUI2
+{
+    public static void main(String A[])
+    {
+        Frame fobj = new Frame("Marvellous PPA");
+
+        fobj.setSize(400,300);
+
+        fobj.setVisible(true);
+
+
+    }
+}
